fd7002097c01894387803efc106f7b97f43eb43d
alaodriss <alaodriss@gmail.com>
2023-04-25 15:49:05 +0200

    create page logement

--- src/components/House/House.tsx
+++ src/components/House/House.tsx
@@ -1,17 +1,15 @@
-import React from 'react'
-import {MOCK_PROJECTS} from '../../Data/MockData'
-import './House.scss'
-import HouseCard from './HouseCard'
-
+import React from "react";
+import { MOCK_PROJECTS } from "../../Data/MockData";
+import "./House.scss";
+import HouseCard from "./HouseCard";
 
 const House = () => {
-  
-    // console.log(MOCK_PROJECTS)
+  // console.log(MOCK_PROJECTS)
   return (
-    <div className='bg_card'>
-<HouseCard  cards={MOCK_PROJECTS}/>
+    <div className="bg_card">
+      <HouseCard cards={MOCK_PROJECTS} />
     </div>
-  )
-}
+  );
+};
 
-export default House
+export default House;
--- src/pages/Logement/LogementData.tsx
+++ src/pages/Logement/LogementData.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { Accommodation } from '../../Data/Data';
-import Page404 from '../Page404/Page404';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Accommodation } from "../../Data/Data";
+import Page404 from "../Page404/Page404";
+import "./Logement.scss";
 
 interface LogPropos {
   data: Accommodation[];
@@ -22,9 +23,34 @@ const LogementData = ({ data }: LogPropos) => {
     return <Page404 />;
   }
 
-  console.log(result)
+  console.log(result);
 
-  return <div>LogInfo</div>;
+  return (
+    <>
+      <div className="InfoLog">
+        <div className="TopInfos">
+          <div className="element">
+            <h1 className="titre_log">{result.title}</h1>
+            <p className="location">{result.location}</p>
+            <div className="tag">
+              {result.tags?.map((item) => (
+                <div className="tags">{item}</div>
+              ))}
+            </div>
+          </div>
+          <div className="element">
+            <p className="name"> {result.host?.name}</p>
+            <img className="picture" src={result.host?.picture} alt="pictur" />
+            <div className="review">
+              {/* {result.rating?.map((ratin) => (
+                <div className="tags">{ratin}</div>
+              ))} */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default LogementData;
